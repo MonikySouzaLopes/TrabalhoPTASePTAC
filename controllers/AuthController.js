db8f252e91693318f8fe01eb9df5d7687f2c0bb8
Maria Eduarda <monikysouzalopes@gmail.com>
2024-10-23 15:28:13 -0300

    aula 23/10

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,25 +1,23 @@
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 
-<<<<<<< HEAD
 
 class AuthController{
     static async cadastro(req, res){}
 
-    static async login(req, res){}
+    static async login(req, res){
+        res.json({
+            email: req.body.email, 
+            senha: req.body.password,
+        });
+    }
+
+
 
     static async loginForm(req, res){
         res.send("<form action='/auth/login'><input type='email' name='email'><input type='submit value='Entrar'></form>"
 
         );
-=======
-class AuthController{
-    static async cadastro(){
-        
-    }
-    static async login(){
-
->>>>>>> 957502786901528976727e1936d391cf21a853bd
     }
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const router = express.Router();
 
@@ -12,18 +12,6 @@ router.post("/cadastro", AuthController.cadastro);
 
 module.exports = router;
 
-=======
-const router = require("express").Router();
 
-const AuthController = require("../controllers/AuthController")
 
-router.post("/cadastro", (req, res) => {AuthController.cadastro});
-router.post("/login", (req, res) => {AuthController.login});
 
-//Responde a qualquer requisição encaminhada para 
-// /auth/algumaCoisa
-const authRoutes = require("./routes/authRoutes");
-app.use("./auth", authRoutes);
-
-module.exports = router;
->>>>>>> 957502786901528976727e1936d391cf21a853bd
