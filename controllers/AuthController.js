d4128ef052e81e2e6d90c4d498a737d8afa6d012
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-10-31 11:07:02 -0400

    Video Aula 5 concluída

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const bcryptjs = require("bcryptjs");
 
 class AuthController{
     static async cadastro(req, res){
@@ -12,15 +13,59 @@ class AuthController{
                 mensagem: "O nome deve ter pelo menos 6 caracteres."
             });
         }
-        return res.json({
-            erro: false,
-            mensagem: "Usuário cadastrado com sucesso!",
-            token: "gfr56789ikjgt",
-        });
-    }
-   
-    static async login(req, res){
+        if(!email || email.length < 10){
+            return res.json({
+                erro: true,
+                mensagem: "O email deve ter pelo menos 10 caracteres."
+            });
+        }
+        if(!password || password.length < 8){
+            return res.json({
+                erro: true,
+                mensagem: "A senha deve ter pelo menos 8 caracteres."
+            });
+        }
 
+        const existe = await prisma.usuario.count({
+            where: {
+                email: email,
+            },
+        });
+
+        if(existe != 0){
+            return res.json({
+                erro: true,
+                mensagem: "Já existe usuário cadastrado com este e-mail"
+            });
+        }
+
+        const salt = bcryptjs.genSaltSync(8);
+        const hashPassword = bcryptjs.hashSync(password, salt);
+
+        try{
+            const usuario = await prisma.usuario.create({
+                data: {
+                    nome: nome,
+                    email: email,
+                    password: hashPassword,
+                    tipo: "cliente",
+                },
+            });
+
+        return res.json({
+        erro: false,
+        mensagem: "Usuário cadastrado com sucesso!",
+       });
+        } catch (error){
+            return res.json({
+                erro: true,
+                mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
+               }); 
+        }
+
+    }
+    static async login(req, res){
+        
     }
     
 }
