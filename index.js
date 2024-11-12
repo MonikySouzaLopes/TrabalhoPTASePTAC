849f73a9687586934288e453a6cdce6e3b9d99a1
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-11-12 16:04:42 -0400

    Atualizado 12.11

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-
+const cors = require("cors");
+app.use(cors());
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
 
 
 
-app.listen(8000);
+app.listen(3000);
